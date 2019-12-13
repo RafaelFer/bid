a262865ebe425ae52af7a8d60a8ec35d2151513f
Rafael Ferreira <rafael22fs@gmail.com>
2019-12-13 16:52:42 -0300

    dados skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout,Button } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Layout>
+    
+      <Header style={{ background: 'Black', padding: 0 }}>
+          <Button style={{ background: '#fffff', padding: 2, margin: 15 }} size="large" icon="home" />
+      </Header>
+      
+      <Content style={{ background: '#fffff', padding: 0 }}>
+      </Content>
+    
+    </Layout>
+  </div>
   );
 }
 
